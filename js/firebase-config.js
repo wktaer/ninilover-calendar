0cca45e2a99672a240095b0bc45e22a02bfa9cf3
wktaer <98446388+wktaer@users.noreply.github.com>
2025-03-18 23:06:48 -0300

    Actualizada configuración de Firebase con nueva API key

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC1wJMB11qR4wFdzrWCV0-eGYq6D9OCcVc",
+    apiKey: "AIzaSyBKHKFIxgvDvRJxBBIVUaJ7OFtFnAcxYZc",
     authDomain: "ninilover-calendar.firebaseapp.com",
-    databaseURL: "https://ninilover-calendar-default-rtdb.firebaseio.com",
     projectId: "ninilover-calendar",
     storageBucket: "ninilover-calendar.appspot.com",
     messagingSenderId: "1063597046269",
-    appId: "1:1063597046269:web:cf3706e14381f6ca5d3c8f"
+    appId: "1:1063597046269:web:cf3706e14381f6ca5d3c8f",
+    measurementId: "G-XXXXXXXXXX"
 };
 
 // Initialize Firebase
